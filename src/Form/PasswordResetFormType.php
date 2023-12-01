c1c2eddee7d10b5c50e6ca42eaf77635763c5832
DayZmooN <karim-du01@live.fr>
2023-12-01 11:40:00 +0100

    ✅ WIP: password request and reset  is Ready

--- src/Form/PasswordResetFormType.php
+++ src/Form/PasswordResetFormType.php
@@ -8,15 +8,16 @@ use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-
 class PasswordResetFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('password')
             ->add('plainPassword', PasswordType::class, [
                 'label' => 'Nouveau mot de passe',
+                'attr' => [
+                    'placeholder' => 'Nouveau mot de passe',
+                ],
             ]);
     }
 
