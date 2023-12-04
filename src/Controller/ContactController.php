3a782a8e52a3aca02640942adf19b9b0d820b949
Angéline <anje.jiro@gmail.com>
2023-12-04 10:28:14 +0100

    🔒️ Contact Form Security update

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -8,35 +8,53 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
+use Symfony\Component\Security\Csrf\CsrfToken;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact/send', name: 'contact_send', methods: "POST")]
-    public function sendContactEmail(Request $request, MailerInterface $mailer)
+    public function sendContactEmail(Request $request, MailerInterface $mailer, ValidatorInterface $validator, CsrfTokenManagerInterface $csrfTokenManager)
     {
+        // Vérifiez le token CSRF
+        $csrfToken = new CsrfToken('contact_form', $request->request->get('_csrf_token'));
+        if (!$csrfTokenManager->isTokenValid($csrfToken)) {
+            $this->addFlash('error', 'Invalid CSRF token.');
+            return $this->redirectToRoute('app_about_us');
+        }
+
         if ($request->isMethod('POST')) {
             $name = $request->request->get('name');
             $senderEmail = $request->request->get('email');
             $phone = $request->request->get('phone');
-            $message = $request->request->get('contact-message');            
-            // Check if senderEmail is not null
+            $message = $request->request->get('contact-message');
 
-            if ($senderEmail === null) {
-                $this->addFlash('error', 'L\'adresse e-mail est obligatoire.');
+            // Validez les données
+            $errors = $validator->validate([
+                'name' => $name,
+                'email' => $senderEmail,
+                'phone' => $phone,
+                'message' => $message,
+            ]);
+
+            // Vérifiez s'il y a des erreurs de validation
+            if (count($errors) > 0) {
+                foreach ($errors as $error) {
+                    $this->addFlash('error', $error->getMessage());
+                }
                 return $this->redirectToRoute('app_about_us');
             }
 
+            // Créez et envoyez l'e-mail
             try {
-                // Create and send the email
                 $email = (new Email())
-                ->from($senderEmail)
-                ->to('gremlins.coders@gmail.com') // Replace with IMS Beauty's email when live
-                ->subject('Contact Form Submission')
-                ->html("<p>Name: $name</p><p>Email: $senderEmail</p><p>Phone: $phone</p><p>Message: $message</p>");
+                    ->from($senderEmail)
+                    ->to('gremlins.coders@gmail.com')// Email d'IMS Beauty à ajouter plus tard
+                    ->subject('Formulaire Contact')
+                    ->html("<p>Name: $name</p><p>Email: $senderEmail</p><p>Phone: $phone</p><p>Message: $message</p>");
 
                 $mailer->send($email);
-
-                // Add a flash message for success
                 $this->addFlash('success', 'Votre message a été envoyé avec succès.');
             } catch (\Exception $e) {
                 $this->addFlash('error', 'Erreur lors de l\'envoi du message: ' . $e->getMessage());
